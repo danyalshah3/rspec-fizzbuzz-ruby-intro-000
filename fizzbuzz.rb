b33f97b83693033e1685493917543d72a0b38e32
Muhammad mahdi <danyalm50@gmail.com>
2020-10-20 18:13:23 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  elsif int % 3 == 0
-    puts "Fizz"
-  elsif int % 5 == 0
-    puts "Buzz"
-  end
+    if int % 3 == 0
+        puts "Fizz"
+    end
+    if int % 5 == 0
+        puts "Buzz"
+    end
+    if (int % 3 == 0) && (int % 5 == 0)
+        puts "FizzBuzz"
+    end
 end
